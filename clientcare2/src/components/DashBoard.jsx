4247ef9f2a3b9ceb7f5b79522a80f3095206c3ed
Esteban Zavala <zavala_esteban4455@yahoo.com>
2024-10-18 19:52:38 -0500

    Create DashBoard.jsx
    Created dashboard.jsx file so phebe can view the reports

---
+++ clientcare2/src/components/DashBoard.jsx
@@ -0,0 +1,9 @@
+// clientcare2/src/components/Dashboard.jsx
+import React from 'react';
+
+const Dashboard = () => {
+  // Placeholder component for Dashboard page
+  return <h1 className="text-2xl font-bold">Dashboard</h1>;
+};
+
+export default Dashboard;
