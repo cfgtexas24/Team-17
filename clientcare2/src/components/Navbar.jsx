5c9d7d17e04fa12c5898b3413b18c4f9c6acdfcc
Esteban Zavala <zavala_esteban4455@yahoo.com>
2024-10-18 19:51:44 -0500

    Update Navbar.jsx
    Updated navbar to have the icons be on the right aside

--- clientcare2/src/components/Navbar.jsx
+++ clientcare2/src/components/Navbar.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaChartBar, FaClipboardList, FaUserCircle } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    // Side navigation container
+    <nav className="fixed top-0 right-0 h-full w-16 bg-gray-800 flex flex-col items-center py-4">
+      {/* Navigation buttons */}
+      <NavButton to="/dashboard" icon={<FaChartBar size={24} />} tooltip="Dashboard" />
+      <NavButton to="/reports" icon={<FaClipboardList size={24} />} tooltip="Reports" />
+      <NavButton to="/profile" icon={<FaUserCircle size={24} />} tooltip="Profile" />
+    </nav>
+  );
+};
 
-export default Navbar
+// Reusable component for navigation buttons
+const NavButton = ({ to, icon, tooltip }) => {
+  return (
+    <Link
+      to={to}
+      className="text-white hover:bg-gray-700 p-3 rounded-full mb-4 relative group"
+    >
+      {icon}
+      <span className="absolute right-full mr-2 bg-gray-700 text-white px-2 py-1 rounded text-xs opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+        {tooltip}
+      </span>
+    </Link>
+  );
+};
+
+export default Navbar;
