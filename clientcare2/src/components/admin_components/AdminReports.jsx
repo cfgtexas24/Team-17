6c4ad5e1b315a27abeb0e3ebc18262634446b2cd
Esteban Zavala <zavala_esteban4455@yahoo.com>
2024-10-19 00:53:11 -0500

    XXXXXXXX

--- clientcare2/src/components/admin_components/AdminReports.jsx
+++ clientcare2/src/components/admin_components/AdminReports.jsx
@@ -4,11 +4,14 @@ import NewForm from './NewForm';
 import CreateForm from './CreateForm';
 import FormDetail from './FormDetail';
 
+import React from 'react';
+import NewForm from './NewForm';
+
 const AdminReports = () => {
   return (
     <div className="h-screen w-full bg-white p-4">
       <h1 className="text-2xl font-bold mb-4">Reports & Forms</h1>
-  
+      <NewForm />
     </div>
   );
 };
