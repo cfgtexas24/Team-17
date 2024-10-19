2ac15e4676f2ad1c663f25e2e77b609fc7bc8ba9
angelali314159 <angelali314159@gmail.com>
2024-10-18 22:00:05 -0400

    set up routing and layout in App component with Navbar and placeholder routes

---
+++ clientcare2/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import UserHome from './components/user_components/UserHome'
+import { Route, Routes } from 'react-router-dom'
+import Navbar from './components/user_components/Navbar'
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<UserHome/>}/>
+        <Route path="/forms" element={<></>}/>
+        <Route path="/dashboard" element={<></>}/>
+        <Route path="/profile" element={<></>}/>
+      </Routes>
+    </>
+  )
+}
+
+export default App
