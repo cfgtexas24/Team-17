1e66625f4ddc5b4c0a3abeda69d052f7ca46d19d
Carlos <hernandezc2@ufl.edu>
2024-10-18 22:31:09 -0400

    Updated Backend Routing backbone based on user/admin roles

---
+++ clientcare2/src/backend/user_auth/routes/authRoutes.ts
@@ -0,0 +1,10 @@
+import {Routes} from "express";
+import {servePage} from "../controllers/authController";
+
+const router = Routes();  // the intialized express router object
+
+router.get("/page/:role", servePage)  // the router will serve the page whenevr the page route with the specific role is called. The controller logic will control what to do based on role
+
+
+// NOTE: for now the page route represents the page you go to after logging in as a user/admin
+export default router;
