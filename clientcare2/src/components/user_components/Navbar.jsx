f7a86bf320b244102731c452cf674c84f8869f83
Safa Mohammed <sumohammed0@gmail.com>
2024-10-19 07:21:59 -0500

    updated home styles

--- clientcare2/src/components/user_components/Navbar.jsx
+++ clientcare2/src/components/user_components/Navbar.jsx
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
     return (
         <div className='flex flex-row justify-between'>
-          <h1 className='text-3xl font-bold text-black'>Client Care 2.0</h1>
+          <h1 className='text-3xl font-bold text-[#3a696e]'>Abide and Seek</h1>
           <div className='flex flex-row justify-evenly'>
-            <Link to="/" className='text-black font-semibold p-2 ml-2 mr-4 hover:text-white hover:bg-slate-400 rounded-lg'>Home</Link>
-            <Link to="/formsPage" className='text-black font-semibold p-2 ml-2 mr-4 hover:text-white hover:bg-slate-400 rounded-lg'>Forms</Link>
-            <Link to="/dashboard" className='text-black font-semibold p-2 ml-2 mr-4 hover:text-white hover:bg-slate-400 rounded-lg'>Dashboard</Link>
-            <Link to="/profile" className='text-black font-semibold p-2 ml-2 mr-2 hover:text-white hover:bg-slate-400 rounded-lg'>Profile</Link>
-            <Link to="/calendar" className='text-black font-semibold p-2 ml-2 mr-2 hover:text-white hover:bg-slate-400 rounded-lg'>Appointment Calendar</Link>
-            <Link to="/patientSearch" className='text-black font-semibold p-2 ml-2 mr-2 hover:text-white hover:bg-slate-400 rounded-lg'>Patient Search</Link>
+            <Link to="/" className='text-[#3d251e] font-semibold p-2 ml-2 mr-4 hover:text-white hover:bg-[#3a696e] hover:bg-opacity-65 rounded-lg'>Home</Link>
+            <Link to="/formsPage" className='text-[#3d251e] font-semibold p-2 ml-2 mr-4 hover:text-white hover:bg-[#3a696e] hover:bg-opacity-65 rounded-lg'>Forms</Link>
+            <Link to="/dashboard" className='text-[#3d251e] font-semibold p-2 ml-2 mr-4 hover:text-white hover:bg-[#3a696e] hover:bg-opacity-65 rounded-lg'>Dashboard</Link>
+            <Link to="/profile" className='text-[#3d251e] font-semibold p-2 ml-2 mr-2 hover:text-white hover:bg-[#3a696e] hover:bg-opacity-65 rounded-lg'>Profile</Link>
+            <Link to="/calendar" className='text-[#3d251e] font-semibold p-2 ml-2 mr-2 hover:text-white hover:bg-[#3a696e] hover:bg-opacity-65 rounded-lg'>Appointment Calendar</Link>
+            <Link to="/patientSearch" className='text-[#3d251e] font-semibold p-2 ml-2 mr-2 hover:text-white hover:bg-[#3a696e] hover:bg-opacity-65 rounded-lg'>Patient Search</Link>
             
           </div>
         </div>
