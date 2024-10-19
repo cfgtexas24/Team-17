d57b2189335b86fe31d690dfaac15537ebb8dc76
Esteban Zavala <zavala_esteban4455@yahoo.com>
2024-10-18 23:21:42 -0500

    its working now

--- clientcare2/src/components/admin_components/AdminReports.jsx
+++ clientcare2/src/components/admin_components/AdminReports.jsx
@@ -1,13 +1,16 @@
-// clientcare2/src/components/Reports.jsx
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import NewForm from './NewForm';
+import CreateForm from './CreateForm';
+import FormDetail from './FormDetail';
 
-const Reports = () => {
+const AdminReports = () => {
   return (
-    <div className="h-screen w-full bg-white">
-      {/* This page is intentionally left blank */}
+    <div className="h-screen w-full bg-white p-4">
+      <h1 className="text-2xl font-bold mb-4">Reports & Forms</h1>
       
     </div>
   );
 };
 
-export default Reports;
+export default AdminReports;
